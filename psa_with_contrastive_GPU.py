4d47c5e60189fe51dce60c8643c8243468923266
rodrigo <rodrigo.moreira206@gmail.com>
2024-11-01 20:42:33 -0300

    working on

--- psa_with_contrastive_GPU.py
+++ psa_with_contrastive_GPU.py
@@ -48,7 +48,7 @@ def contrastive_loss(h_i, h_j, temperature=0.5):
 #---------------------------------#
 
 # Leitura e pré-processamento do CSV
-df_may = pd.read_csv("dataset/may.csv")
+df_may = pd.read_csv("dataset/august.csv")
 #df_august = pd.read_csv("dataset/agosto.csv", skiprows=1, on_bad_lines='warn')
 #df = pd.concat([df_may, df_august], ignore_index=True)
 df = df_may.drop(columns=['fecha_esp32', 'weekday', 'MAC'])
